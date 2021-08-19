c07d58194be03278dd4e349f27a52893539de57b
Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
2021-08-19 16:52:33 +0200

    Update ansys/tools/example_coverage/_version.py
    Co-authored-by: Alex Kaszynski <akascap@gmail.com>

--- ansys/tools/example_coverage/_version.py
+++ ansys/tools/example_coverage/_version.py
@@ -1,6 +1,6 @@
 """Version of ``ansys-tools-example-coverage`` module.
 
-On the ``master`` branch, use 'dev0' to denote a development version.
+On the ``main`` branch, use 'dev0' to denote a development version.
 For example:
 
 version_info = 0, 58, 'dev0'
