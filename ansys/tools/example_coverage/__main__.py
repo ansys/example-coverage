84f2f0205d68c4cef90e887dc6a23607afb063da
Maxime Rey <maxime.rey@ansys.com>
2021-08-30 17:11:05 +0200

    Fix E128 continuation line under-indented in the __main__.py

--- ansys/tools/example_coverage/__main__.py
+++ ansys/tools/example_coverage/__main__.py
@@ -12,6 +12,6 @@ from .example_coverage import create_report
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Evaluate example coverage of a package.')
     parser.add_argument('-f', '--folder',
-        help='path of the package to perform coverage analysis on')
+                        help='path of the package to perform coverage analysis on')
     args = parser.parse_args()
     create_report(args.folder)
