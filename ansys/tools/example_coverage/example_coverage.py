747589d6d83d5565f673c07c3afda6d9be1c115f
Maxime Rey <maxime.rey@ansys.com>
2021-08-30 16:33:04 +0200

    Add flake8 linter in the ci/cd and reduce code line length to a maximum of 100 chars.

--- ansys/tools/example_coverage/example_coverage.py
+++ ansys/tools/example_coverage/example_coverage.py
@@ -1,7 +1,5 @@
 import ast
 import os
-import sys
-import argparse
 import pathlib
 
 
@@ -16,7 +14,8 @@ def find_files(directory_path):
     if not os.listdir(directory_path):
         raise FileNotFoundError(f"There are no python source files in {directory_path}.")
 
-    modules = [str(path) for path in pathlib.Path(directory_path).rglob('*.py') if not str(path).endswith("__init__.py")]
+    modules = [str(path) for path in pathlib.Path(directory_path).rglob('*.py')
+               if not str(path).endswith("__init__.py")]
 
     if not modules:
         raise FileNotFoundError(f"No python modules found in: {directory_path}.")
@@ -28,7 +27,7 @@ def create_report(directory_path):
     """Write a report to list all modules and the docstring example
     coverage stats for each of these modules."""
     print(f'{"Name": <43}{"Docstrings":>12}{"Missed":>11}{"Covered":>10}')
-    print ('-' * 79)
+    print('-' * 79)
 
     # The key of those dictionaries will be the name of the file.
     # So we can easily extract specific information for every module.
@@ -48,7 +47,10 @@ def create_report(directory_path):
             # private function are not expected to provide any examples
             if function.name.startswith('_'):
                 continue
-            if (ast.get_docstring(function) is None) or ("Example" not in ast.get_docstring(function)):
+            if (
+                    (ast.get_docstring(function) is None) or
+                    ("Example" not in ast.get_docstring(function))
+               ):
                 missing_functions.append(function.name)
             else:
                 covered_functions.append(function.name)
@@ -59,8 +61,12 @@ def create_report(directory_path):
         missing_classes = []
         covered_classes = []
         for class_def in class_definitions:
-            method_definitions.extend([node for node in class_def.body if isinstance(node, ast.FunctionDef)])
-            if (ast.get_docstring(class_def) is None) or ("Example" not in ast.get_docstring(class_def)):
+            method_definitions.extend([node for node in class_def.body
+                                      if isinstance(node, ast.FunctionDef)])
+            if (
+                    (ast.get_docstring(class_def) is None) or
+                    ("Example" not in ast.get_docstring(class_def))
+               ):
                 missing_classes.append(class_def.name)
             else:
                 covered_classes.append(class_def.name)
@@ -105,7 +111,8 @@ def create_report(directory_path):
                     covered_methods.append(method.name)
 
         # Write report.
-        all_methods_without_example[file_name] = missing_functions + missing_classes + missing_methods
+        all_methods_without_example[file_name] = missing_functions + missing_classes + \
+            missing_methods
         all_methods_with_example[file_name] = covered_functions + covered_classes + covered_methods
 
         missing = len(all_methods_without_example[file_name])
@@ -118,7 +125,9 @@ def create_report(directory_path):
             # If no docstring is available in the module, coverage is considered to be 100%.
             percentage_covered = 100
 
-        module_name = os.path.basename(directory_path) + pathlib.Path(file_name.split(directory_path)[1].replace(os.path.sep, ".")).stem
+        module_name = (
+            os.path.basename(directory_path) +
+            pathlib.Path(file_name.split(directory_path)[1].replace(os.path.sep, ".")).stem)
 
         print(f'{module_name[:42]: <43}{total:12}{missing:11}{percentage_covered:9.1f}%')
 
@@ -134,10 +143,11 @@ def create_report(directory_path):
     package_total = len(all_methods_with_example_list) + len(all_methods_without_example_list)
     package_missing = len(all_methods_without_example_list)
 
-    if package_total!=0:
+    if package_total != 0:
         package_percentage_covered = (package_total - package_missing) / package_total * 100
     else:
         # If none example is expected in the entire package, coverage is considered to be 100%.
         package_percentage_covered = 100
-    print ('-' * 79)
-    print(f'{"Total" : <43}{package_total : 12d}{package_missing : 11d}{package_percentage_covered:9.1f}%')
+    print('-' * 79)
+    print(f'{"Total" : <43}{package_total : 12d}{package_missing : 11d}'
+          f'{package_percentage_covered:9.1f}%')
