c2995469e689bf24184bcab810332707872b7b67
Maxime Rey <maxime.rey@ansys.com>
2021-08-30 16:58:20 +0200

    Remove trailing whitespace.

--- tests/assets/module_b/module_b.py
+++ tests/assets/module_b/module_b.py
@@ -4,7 +4,7 @@ Only for testing purpose.
 
 
 def _private_testing_function():
-    """This function is private, so its example will not be 
+    """This function is private, so its example will not be
     included into the covering report.
 
     Examples
